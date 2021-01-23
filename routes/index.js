fe3f4da74105b44801a05819d3d81302147a8b0f
Gurkengewuerz <niklas@schuetrumpf.net>
2021-01-23 02:17:21 +0100

    fixed file/folder creation

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 const router = express.Router();
 
 router.post('/:name/:type', async (req, res, next) => {
     const dir = `./${req.params.name}/${req.params.type}`;
-    const unix = Math.floor(new Date().getTime() / 1000);
-    if (!fs.existsSync(`./${req.params.name}/`)) fs.mkdirSync(dir, {recursive: true});
-    fs.writeFile(`${dir}/${unix}.json`, JSON.stringify(req.body), function (err) {
+    const unix = Math.floor((new Date().getTime()) / 1000);
+    if (!fs.existsSync(`data/${dir}`)) fs.mkdirSync(`data/${dir}`, {recursive: true});
+    fs.writeFile(`data/${dir}/${unix}.json`, JSON.stringify(req.body), function (err) {
         if (err) throw err;
     }); 
 
